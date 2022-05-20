bdb1315764ae75fe76c00029cfbf0b8f6de61472
Arseniy Graur <argraur@gmail.com>
2022-05-20 23:14:29 +0300

    Deploy web server through ktor

--- src/main/kotlin/org/ciphen/polyhoot/services/Routing.kt
+++ src/main/kotlin/org/ciphen/polyhoot/services/Routing.kt
@@ -1,12 +1,13 @@
 package org.ciphen.polyhoot.services
 
 import io.ktor.server.application.*
+import io.ktor.server.http.content.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
-//        singlePageApplication {
-//            react("/home/vladdenisov/projects/web2/build")
-//        }
+        singlePageApplication {
+            react("../web/build")
+        }
     }
 }
